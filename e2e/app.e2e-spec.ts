a6b1cf4d7aa75d6960b09e7653e36fd54ac17193
oscar callisaya <oreynaldo.cl@gmail.com>
2018-05-11 02:07:54 -0400

    Renamed package to github summary.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('config-app App', () => {
+describe('github-summary App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
